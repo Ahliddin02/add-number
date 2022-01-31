f20ffd3862ae955cfaa2d3f1f2f5cc2a73bce340
Ahliddin Mazbutov <ahliddin.mazbutov@gmail.com>
2022-01-31 18:15:03 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
 
 function App() {
+
+  // let numbers = [1, 555, 3];  
+  const [numbers, setNumbers] = React.useState([1, 555,3]);  
+
+  const addNumber = () => {
+    const randNumber = Math.round(Math.random() * 10);
+    const newArr = [...numbers, randNumber];
+    setNumbers(newArr);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <ul>
+        {numbers.map((num, index) => (
+          <li key={index}>{num}</li>
+        ))}
+       </ul>
+       <button onClick={addNumber}>Новое число</button>
     </div>
   );
 }
